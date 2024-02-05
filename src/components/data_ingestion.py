3d8ab67ab22a664ea5a78b627c69f3625f167fbf
HariprashaadSR035 <hariprashaadsrofficial@gmail.com>
2024-02-05 19:47:06 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_path = os.path.join('artifacts','train.csv')
+    test_path = os.path.join('artifacts','test.csv')
+    raw_path = os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.paths = DataIngestionConfig()
+
+    def initialise_Ingestion(self):
+        logging.info('Data ingestion started')
+        try:
+            df = pd.read_csv('data.csv')
+            logging.info('Data Read Successfully')
+
+            os.makedirs(os.path.dirname(self.paths.train_path), exist_ok = True)
+            
+            df.to_csv(self.paths.raw_path, index = False, header = True)
+
+            train, test = train_test_split(df, test_size = 0.2, random_state = 2)
+            logging.info('Train test split initiated')
+            
+            train.to_csv(self.paths.train_path, index = False, header = True)
+            test.to_csv(self.paths.test_path, index = False, header = True)
+
+            logging.info('Data ingestion completed')
+            return (self.paths.train_path, self.paths.test_path)
+        
+        except Exception as e:
+            logging.info('Exception occured')
+            raise CustomException(e, sys)
+        
+
+if __name__ == '__main__':
+    data = DataIngestion()
+    data.initialise_Ingestion()
+        
+
--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error_message, error_detail : sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -19,4 +20,5 @@ if __name__ == '__main__':
     try:
         a = 1/0
     except Exception as e:
+        logging.info('Exception has occured')
         raise CustomException(e, sys)
